15a703050c33a3cdcdb98a2cb094c9ae83714787
rbm92 <ricard.bargallo.m@gmail.com>
2021-12-13 09:41:02 +0100

    Añadido middleware guard y control errores

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,8 @@ async function bootstrap() {
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
-  await app.listen(3000)
-  // await app.listen(process.env.PORT)
+  // await app.listen(3000)
+  await app.listen(process.env.PORT)
 }
 
 bootstrap()
